651b9ec9db03f41179fe580e02fef59b320187d1
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-03-29 13:42:10 +0200

    Create start.js

---
+++ start.js
@@ -0,0 +1,12 @@
+"This is an line"
+const firstOne = "This is the valule"
+console.log(firstOne);
+const firstName = "Mario"
+const lastName = "Quezada"
+console.log(firstName, lastName);
+const fName = "Tito"
+const lName = "Jackson"
+const age = "67"
+const city = "Gary"
+const job = "Musician"
+console.log("this is " + fName, lName, "he is a famous " + job, "from " + city, "he is " + age, "years old.");
